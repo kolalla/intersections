e23fa0f81df05626f3ab5574f121dbe00c052cf5
kac624 <121123915+kac624@users.noreply.github.com>
2024-09-28 13:12:59 -0400

    reverse geocode updates

--- scripts/rev_geocode_intersections.py
+++ scripts/rev_geocode_intersections.py
@@ -4,7 +4,7 @@ import time
 import json
 import os
 from datetime import datetime, timedelta
-from multiprocessing import Pool, cpu_count
+# from multiprocessing import Pool, cpu_count
 
 # API KEYS
 with open('data/api_keys.json', 'r') as file:
@@ -23,7 +23,7 @@ def wait_until_next_hour():
     now = datetime.now()
     
     # Calculate the next hour
-    next_hour = (now + timedelta(hours=1)).replace(minute=1, second=0, microsecond=0)
+    next_hour = (now + timedelta(hours=1)).replace(minute=5, second=0, microsecond=0)
     
     # Calculate the sleep duration in seconds
     sleep_duration = (next_hour - now).total_seconds()
@@ -34,9 +34,22 @@ def wait_until_next_hour():
 
 def check_for_intersection(coords, distance_threshold=0.02, api_key=geonames_username):
     url = f"http://api.geonames.org/findNearestIntersectionJSON?lat={coords[0]}&lng={coords[1]}&username={api_key}"
-    
+    attempt = 0
+    backoff_factor = 2
+    max_attempts = 5
+
     while True:  # Infinite loop to retry the same record if limit is hit
-        response = requests.get(url)
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+        except (requests.exceptions.ConnectionError, requests.exceptions.HTTPError) as e:
+            if attempt >= max_attempts:
+                print(f"Maximum number of attempts reached for {coords}.")
+                return coords[0], coords[1], False, None
+            print(f"Error encountered: {e}. Retrying in {backoff_factor * (2 ** attempt)} seconds...")
+            time.sleep(backoff_factor * (2 ** attempt))  # Exponential backoff
+            attempt += 1
+        
         if response.status_code == 200:
             # Process the response
             data = response.json()
@@ -59,11 +72,57 @@ def check_for_intersection(coords, distance_threshold=0.02, api_key=geonames_use
         # Return false / None if no data found
         return coords[0], coords[1], False, None
 
-def pool_check_for_intersection(coords):
-    pool_size = cpu_count()
-    with Pool(pool_size) as p:
-        results = p.map(check_for_intersection, coords)
-    return results
+# DATA
+df = pd.read_csv(REFERENCE_FILE, low_memory=False)
+coords = df[['LATITUDE', 'LONGITUDE']].values.tolist()
+
+# Load previously processed coordinates (if any)
+previously_processed = os.path.isfile('data/processed/intersection_flags.csv')
+
+if previously_processed:
+    previously_processed = pd.read_csv('data/processed/intersection_flags.csv')
+    previously_processed = previously_processed[~pd.isna(previously_processed['data'])]
+    processed_coords = [tuple(coord) for coord in previously_processed[['lat', 'lng']].values.tolist()]
+else:
+    df_template = pd.DataFrame(columns=['lat', 'lng', 'intersection', 'data'])
+    df_template.to_csv('data/processed/intersection_flags.csv', index=False, header=True)
+    processed_coords = []
+
+# Filter out already processed coordinates
+coords_to_process = list(set([tuple(coord) for coord in coords if tuple(coord) not in processed_coords]))
+if NUM_RECORDS_TO_PROCESS:
+    coords_to_process = coords_to_process[0:NUM_RECORDS_TO_PROCESS]
+
+# RUN
+if coords_to_process:
+    results = []
+    for i, coord in enumerate(coords_to_process):
+        # Check for intersection and add to results
+        result = check_for_intersection(coord)
+        results.append(result)
+        # Write results to file
+        if i % 100 == 0:
+            results_df = pd.DataFrame(results, columns=['lat', 'lng', 'intersection', 'data'])
+            results_df.to_csv('data/processed/intersection_flags.csv', index=False, header=False, mode='a')
+            results = []
+        # Print progress
+        if i % 1000 == 0:
+            print(f"Processed {i} records...")
+    # Write remaining results to file
+    if results:
+        results_df = pd.DataFrame(results, columns=['lat', 'lng', 'intersection', 'data'])
+        results_df.to_csv('data/processed/intersection_flags.csv', index=False, header=False, mode='a')
+else:
+    print("All coordinates have already been processed.")
+
+
+
+
+# def pool_check_for_intersection(coords):
+#     pool_size = cpu_count()
+#     with Pool(pool_size) as p:
+#         results = p.map(check_for_intersection, coords)
+#     return results
 
 # if __name__ == "__main__":
 #     # DATA
@@ -98,37 +157,3 @@ def pool_check_for_intersection(coords):
     
 #     else:
 #         print("All coordinates have already been processed.")
-
-# DATA
-df = pd.read_csv(REFERENCE_FILE, low_memory=False)
-coords = df[['LATITUDE', 'LONGITUDE']].values.tolist()
-
-# Load previously processed coordinates (if any)
-previously_processed = os.path.isfile('data/processed/intersection_flags.csv')
-
-if previously_processed:
-    previously_processed = pd.read_csv('data/processed/intersection_flags.csv')
-    previously_processed = previously_processed[~pd.isna(previously_processed['data'])]
-    processed_coords = [tuple(coord) for coord in previously_processed[['lat', 'lng']].values.tolist()]
-    add_header = False
-else:
-    processed_coords = []
-    add_header = True
-
-# Filter out already processed coordinates
-coords_to_process = list(set([tuple(coord) for coord in coords if tuple(coord) not in processed_coords]))
-if NUM_RECORDS_TO_PROCESS:
-    coords_to_process = coords_to_process[0:NUM_RECORDS_TO_PROCESS]
-
-# RUN
-if coords_to_process:
-    results = []
-    for i, coord in enumerate(coords_to_process):
-        results.append(check_for_intersection(coord))
-    results_df = pd.DataFrame(results, columns=['lat', 'lng', 'intersection', 'data'])
-
-    # OUTPUT
-    results_df.to_csv('data/processed/intersection_flags.csv', index=False, header=add_header, mode='a')
-
-else:
-    print("All coordinates have already been processed.")
